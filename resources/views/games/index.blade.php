d570a38f848ddae3684c78c6607c1aea81b78958
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-01-29 14:37:44 +0100

    changed order in games section (title > image > desc) added title

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -10,23 +10,22 @@
 
 @include('partials.header')
 
-<x-content-styles class="flex flex-col max-w-7xl"> 
+<x-content-styles class="flex flex-col max-w-7xl">
+<h1 class="text-h1"><strong>Games</strong></h1><br>
   <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-    @foreach ($games as $game)
-      <li class="bg-white shadow-md rounded-lg p-4 flex items-center space-x-4 border-2 border-orange-500">
-        <img src="https://via.placeholder.com/150"
+      @foreach ($games as $game)
+      <li class="bg-white shadow-md rounded-lg p-4 flex flex-col items-center justify-between border-2 border-orange-500 h-[350px]">
+        <h2 class="text-gray-800 hover:text-blue-500 text-xl font-bold text-center">
+          {{ $game->title }}
+        </h2>
+        <img src="https://via.placeholder.com/200" 
         alt="Placeholder image"
-        class="w-16 h-16 object-cover rounded-lg">
-        <div>
-          <p class="text-gray-800 hover:text-blue-500 text-xl mb-2 font-bold">
-            {{ $game->title }}
-          </p>
-          <p class="text-gray-600">
-            {{ $game->description }}
-          </p>
-        </div>
+        class="w-64 h-64 object-cover rounded-lg">
+        <p class="text-gray-600">
+          {{ $game->description }}
+        </p>
       </li>
-    @endforeach
+      @endforeach
   </ul>
 
   <x-button-styles size="small" class="max-w-xs mt-6">
