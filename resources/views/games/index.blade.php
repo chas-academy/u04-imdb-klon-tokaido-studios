ca8eab895a80978a5758133109c38fe1f1270032
Viktor <viktor.ekstrom@chasacademy.se>
2025-01-30 21:01:45 +0100

    added images and trailers in GameSeeder.php file, added images in public catalog, modified games view for testing purpose

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,5 +1,20 @@
-<ul>
+
 @foreach ($games as $game)
-    <li>Gametitle: {{ $game->title }} | Game description: {{ $game->description }}</li>
+    <div style="display: flex; gap: 10px; align-items: center; margin-bottom: 5px; font-size: 24px; color: #B8860B;">
+        <img src="{{ asset($game->image) }}" alt="{{ $game->title }}" width="50" height="50"> 
+        {{ $game->title }} | Game description: {{ $game->description }}
+    </div>
+    <div style="margin-bottom: 20px;">
+        @php
+            $videoId = str_replace('https://youtu.be/', '', $game->trailer);
+        @endphp
+        <iframe 
+            width="280" 
+            height="158" 
+            src="https://www.youtube.com/embed/{{ $videoId }}" 
+            frameborder="0" 
+            allow="autoplay; encrypted-media" 
+            allowfullscreen>
+        </iframe>
+</div>
 @endforeach
-</ul>
