e6bdd4448400a86280f3167e8e6d8de9b349ca0c
Wille1989 <W.Lundqvist@live.se>
2025-01-30 11:28:07 +0100

    Added breeze routes, removed testLayout as its not needed

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,21 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\GenreController;
 
+/* LÄMNAS UT KOMMENTERAT FÖR FRAMTIDA IMPELEMENTERING */
+/*
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+require __DIR__.'/auth.php';
+*/
+
+
+
 // Förstasidan där sökfältet och länkar till 3 första routes nedan kan finnas
 Route::get('/', function () {
     return view('home');
