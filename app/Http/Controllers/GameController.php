0f07b81223f88efdc5b636912dc4b9c1b642594f
Viktor <viktor.ekstrom@chasacademy.se>
2025-01-30 13:43:00 +0100

    added models with relations between the tables

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -20,20 +20,5 @@ class GameController extends Controller
         $games = Game::where('title', 'like', '%' . $title . '%')->get();
         return view('games.search', ['games' => $games]);
     }
-
-    public function show($id)
-    {
-        $game = Game::with(['reviews.user', 'lists'])->findOrFail($id);
-        return view('games.show', ['game' => $game]);
-    }
-
-    public function addToList(Request $request, $id)
-    {
-        $listId = $request->input('listID');
-        
-        $game = Game::findOrFail($id);
-        $game->lists()->attach($listId);
-
-        return redirect()->route('games.show', $id)->with('success', 'Spelet har lagts till i listan!');
-    }
+    
 }
--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -8,38 +8,5 @@ use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    public function store(Request $request, Game $game)
-    {
-        $validated = $request->validate([
-            'Title' => 'required|max:255',
-            'description' => 'nullable',
-        ]);
-
-        $review = $game->reviews()->create([
-            'Title' => $validated['Title'],
-            'description' => $validated['description'],
-            'userID' => auth()->id(),
-        ]);
-
-        return redirect()->route('games.show', $game);
-    }
-
-    public function update(Request $request, Review $review)
-    {
-        $validated = $request->validate([
-            'Title' => 'required|max:255',
-            'description' => 'nullable',
-        ]);
-
-        $review->update($validated);
-
-        return redirect()->route('games.show', $review->game);
-    }
-
-    public function destroy(Review $review)
-    {
-        $game = $review->game;
-        $review->delete();
-        return redirect()->route('games.show', $game);
-    }
+    //
 }
