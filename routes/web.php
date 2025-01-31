daf90b1abb1526c7cc4dae264f0d8f0eef9e454e
Viktor <viktor.ekstrom@chasacademy.se>
2025-01-31 12:13:05 +0100

    added functions for creating, updating and deleting a game in GameController.php. Added new routes for those functions, added views for those routes.

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\GenreController;
 
-/* LÄMNAS UT KOMMENTERAT FÖR FRAMTIDA IMPELEMENTERING */
-/*
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-require __DIR__.'/auth.php';
-*/
-
-
-
 // Förstasidan där sökfältet och länkar till 3 första routes nedan kan finnas
 Route::get('/', function () {
     return view('home');
@@ -36,3 +21,23 @@ Route::get('/genres/{id}/games', [GenreController::class, 'showGames'])->name('g
 
 // Route för search
 Route::get('/search', [GameController::class, 'search'])->name('search');
+
+// Nya routes för GameController
+Route::get('/games/create', [GameController::class, 'createGame'])->name('games.create');
+Route::post('/games', [GameController::class, 'storeGame'])->name('games.store');
+Route::get('/games/{gameID}/edit', [GameController::class, 'editGame'])->name('games.edit');
+Route::put('/games/{gameID}', [GameController::class, 'updateGame'])->name('games.update');
+Route::delete('/games/{gameID}', [GameController::class, 'deleteGame'])->name('games.destroy');
+
+/* LÄMNAS UT KOMMENTERAT FÖR FRAMTIDA IMPELEMENTERING */
+/*
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+require __DIR__.'/auth.php';
+*/
