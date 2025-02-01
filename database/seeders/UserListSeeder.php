621220857a07640c9aa0f28e3c3265cf3018ed58
Josefine klasson <jossanwowar93@gmail.com>
2025-02-01 16:52:54 +0100

    fixade PSR-12 i UserListSeeder

--- database/seeders/UserListSeeder.php
+++ database/seeders/UserListSeeder.php
@@ -11,31 +11,31 @@ class UserListSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    { // lista med olika listor
-       $lists [
-        "listID" => 1,
-            [
-                "listname" => "Games I want to buy",
-                "description" => "A list of games I want to buy"
-                "userID" => 1,
-                "games" => [2,4,6,10]
-            ],
-        "listID" => 2,
-            [
-                "listname" => "My favourites",
-                "description" => "My favourites games list"
-                "userID" => 4,
-                "games" => [1,3,5,8]
-            ],
-        "listID" => 3,
+    {
+        // lista med olika listor
+       $lists = [
         [
+            "listID" => 1,
+            "listname" => "Games I want to buy",
+            "description" => "A list of games I want to buy",
+            "userID" => 1,
+            "games" => [2, 4, 6, 10]
+        ],
+        [
+            "listID" => 2,
+            "listname" => "My favourites",
+            "description" => "My favourites games list",
+            "userID" => 4,
+            "games" => [1, 3, 5, 8]
+        ],
+        [
+            "listID" => 3,
             "listname" => "Top 20 games", // lägger till fler games när det finns mer spel
-            "description" => "Top 20 games to play"
-            "userID" => null // ingen User
-            "games" = [7,9,11,12] // lägger till 4st sålänge till lägger till fler spel i db
-        ]
-            
-            ];
+            "description" => "Top 20 games to play",
+            "userID" => null, // ingen User
+            "games" => [7, 9, 11, 12] // lägger till 4st sålänge till lägger till fler spel i db
+        ],
+    ];
                     // Skapa eller uppdatera listor i databasen
         foreach ($lists as $list) {
             $userList = UserList::updateOrCreate(
