2ee2fea7dc6d08a8153d6d61fa5ae65b0869ed46
Wille1989 <W.Lundqvist@live.se>
2025-02-05 08:39:47 +0100

    Added clarification for requierments when register a new user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,9 +11,11 @@
 
 <label for="password">Password:</label>
 <input type="password" name="password" class="mt-2"><br>
+<p class="text-p text-xs">Minst 8 bokstäver</p>
 
 <label for="username">Username:</label>
 <input type="text" name="username" class="mt-2"><br>
+<p class="text-p text-xs">Minst 5 bokstäver</p>
 
 <x-button-styles type="submit" class="mt-4">Registrera</x-button-styles>
 </form>
