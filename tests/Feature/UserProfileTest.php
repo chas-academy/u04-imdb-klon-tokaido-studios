2dde93fc14c8674d945bca48b20a8e4fbf95afe4
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-02-07 12:13:50 +0100

    UserProfileTest.php created, code written

---
+++ tests/Feature/UserProfileTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class UserProfileTest extends TestCase {
+    use RefreshDatabase;
+
+    public function userViewProfile() {
+        $user = new User();
+        $user->username = 'testuser';
+        $user->email = 'testuser@example.com';
+        $user->password = Hash::make('password123');
+        $user->country = 'Sweden';
+        $user->isAdmin = false;
+        $user->save();
+
+        $this->actingAs($user);
+
+        $response = $this->get(route('profile.edit'));
+        $response->assertStatus(200);
+        $response->assertSee($user->username);
+        $response->assertSee($user->email);
+    }
+}
