3715f9aaf40b320f4d959ed43f2d40adeb27eb33
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-02-07 09:58:55 +0100

    renamed NewTests.php to CreateGameTest.php

--- tests/Feature/CreateGameTest.php
+++ tests/Feature/CreateGameTest.php
@@ -7,7 +7,7 @@ use App\Models\Genre;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class NewTests extends TestCase {
+class CreateGameTest extends TestCase {
 
     use RefreshDatabase;
 
