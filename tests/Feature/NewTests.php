84e83c1391dc39073318165d37418a58b760be93
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-02-07 09:43:50 +0100

    New attempt at writing tests started

---
+++ tests/Feature/NewTests.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace tests\Feature\Auth;
+
+use App\Models\Game;
+use App\Models\Genre;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class createGameTest extends TestCase {
+    use RefreshDatabase;
+
+    public function testCreateAGame() {
+        $genre = Genre::create([
+            'name' => 'Action',
+        ]);
+
+        $gameData = [
+            'title' => 'Test Game',
+            'description' => 'Beskrivning av Test Game.',
+            'image' => 'https://example.com/image.jpg',
+            'trailer' => 'https://example.com/trailer.mp4',
+            'genres' => [$genre->genreID],
+        ];
+
+        $response = $this->post(route('games.store'), $gameData);
+
+        $response->assertRedirect(route('games.index'));
+
+        $this->assertDatabaseHas('games', [
+            'title' => 'Test Game',
+            'description' => 'Beskrivning av Test Game.',
+            'image' => 'https://example.com/image.jpg',
+            'trailer' => 'https://example.com/trailer.mp4',
+        ]);
+
+        $game = Game::where('title', 'Test Game')->first();
+        $this->assertTrue($game->genres->contains($genre));
+    }
+}
