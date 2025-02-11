e382760655d4e14ada84574fea264b8e05349a4e
Joakim Helgodt <56871857+jhelgodt@users.noreply.github.com>
2025-02-11 16:37:43 +0100

    Update reviews.blade.php

--- resources/views/users/reviews.blade.php
+++ resources/views/users/reviews.blade.php
@@ -9,7 +9,7 @@
         @foreach($reviews as $review)
             <div class="bg-white shadow-md rounded-lg p-6 mb-8">
                 <h1 class="flex items-center text-2xl font-semibold mb-6">
-                    <img src="{{ asset($review->game->image) }}"
+                   <img src="{{ asset('images/games/' . basename($review->game->image)) }}"
                      alt="{{ $review->game->title }}" 
                      class="w-16 h-16 object-cover rounded-lg mr-4">
                     <span>{{ $review->game->title }}</span>
