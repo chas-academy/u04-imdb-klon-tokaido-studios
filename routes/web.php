d0073167ce993627ebbb8810d13fa36d0072d939
Joakim Helgodt <56871857+jhelgodt@users.noreply.github.com>
2025-02-12 18:21:48 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -174,8 +174,7 @@ Route::middleware(UserMiddleware::class)->group(function ()
     // ADMIN BEHÖRIGHETER
 Route::middleware(['auth', AdminMiddleware::class])->group(function()
 {
-    Route::delete('/users/{id}', [AdminController::class, 'destroy'])
-    ->name('users.destroy');
+    
     
     Route::prefix('games')->group(function()
     {
